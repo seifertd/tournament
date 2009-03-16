3ac577c9c57b34f060b5ee578acd05e656f64fad
Douglas A. Seifert <doug@dseifert.net>
2009-03-16 20:07:38 +0000

    Fix bug with changing entry name after entry is completed.  Add test for the bug.

--- webgui/app/models/entry.rb
+++ webgui/app/models/entry.rb
@@ -4,6 +4,7 @@ class Entry < ActiveRecord::Base
   validates_uniqueness_of :name
   validates_presence_of :tie_break
   validates_presence_of :user_id
+  attr_accessor :old_name
 
   # Override bracket to resolve the db blob to an object
   def bracket
@@ -30,8 +31,21 @@ class Entry < ActiveRecord::Base
 
   def after_save
     if self.bracket.complete? && self.user_id != self.pool.user_id
+      if self.old_name
+        # We changed entry names, so pull the old one out of the
+        # backing pool
+        self.pool.pool.entries.delete_if{|e| self.old_name == e.name}
+      end
       self.pool.pool.update_entry(self.tournament_entry)
       self.pool.save!
+    end
+  end
+
+  # Override name= to record name changes
+  def name=(new_name)
+    if new_name != self.name
+      self.old_name = self.name
+      self.write_attribute(:name, new_name)
     end
   end
 
--- webgui/test/functional/entry_controller_test.rb
+++ webgui/test/functional/entry_controller_test.rb
@@ -24,4 +24,31 @@ class EntryControllerTest < ActionController::TestCase
     assert_redirected_to :action => 'show'
     assert_equal "You can't make changes to your entry, the pool has already started.", flash[:error]
   end
+
+  test "entry can change name" do
+    login_as :quentin
+    pool = Pool.find(2)
+    # SHould have 1 pending entry 
+    assert_equal 1, pool.pending_entries.size, "Should have 1 pending entry"
+    assert_equal 1, pool.user_entries.size, "Should have 1 user entry"
+    post(:edit, {:picks => "111222222222222222222222222222222222222222222222222222222222222",
+      :id => 1, :entry => {:name => 'Test', :tie_break => 42}, :pool_id => 2})
+    # Should have 1 completed entry
+    pool = Pool.find(2)
+    assert_equal 0, pool.pending_entries.size, "Should have 0 pending entries."
+    assert_equal 1, pool.user_entries.size, "Should have 1 user entry."
+    assert_equal pool.user_entries.size, pool.pool.entries.size, "AR and backing pool should have same number of entries"
+    assert_equal ['Test'], pool.user_entries.map{|e| e.name}
+    assert_equal ['Test'], pool.pool.entries.map{|e| e.name}
+
+    # Change entry name
+    post(:edit, {:picks => "111222222222222222222222222211111222222222222222222222222222222",
+      :id => 1, :entry => {:name => 'New Name', :tie_break => 42}, :pool_id => 2})
+    pool = Pool.find(2)
+    assert_equal 0, pool.pending_entries.size, "Should have 0 pending entries."
+    assert_equal 1, pool.user_entries.size, "AR pool should have 1 user entry."
+    assert_equal 1, pool.pool.entries.size, "Backing pool should have 1 user entry."
+    assert_equal ['New Name'], pool.user_entries.map{|e| e.name}
+    assert_equal ['New Name'], pool.pool.entries.map{|e| e.name}
+  end
 end
