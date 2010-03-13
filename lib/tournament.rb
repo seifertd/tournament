e5cf68479688e9793a537a45e9342aa9cb43c02c
Douglas A. Seifert <doug@dseifert.net>
2010-03-13 08:44:52 -0800

    Release 3.1.1

--- lib/tournament.rb
+++ lib/tournament.rb
@@ -7,7 +7,7 @@ unless defined? Tournament
 module Tournament
 
   # :stopdoc:
-  VERSION = '3.1.0'
+  VERSION = '3.1.1'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   # :startdoc:
