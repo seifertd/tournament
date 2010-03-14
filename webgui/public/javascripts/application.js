e1fa83a10d6600521414836ba387873c11fea528
Douglas A. Seifert <doug@dseifert.net>
2010-03-13 17:43:19 -0800

    fix.  javascript array swap doesn't create a copy.

--- webgui/public/javascripts/application.js
+++ webgui/public/javascripts/application.js
@@ -17,18 +17,18 @@ var swapMethod = {
 Element.addMethods(swapMethod);
 
 var swaps = [
-  [2,16],
-  [3,8],
-  [4,9],
-  [6,12],
-  [7,4],
-  [3,13],
-  [7,6],
-  [10,11],
-  [10,3],
-  [7,14],
+  [15,2],
   [10,7],
-  [15,2]
+  [7,14],
+  [10,3],
+  [10,11],
+  [7,6],
+  [3,13],
+  [7,4],
+  [6,12],
+  [4,9],
+  [3,8],
+  [2,16]
 ];
 var order = 'seed';
 
@@ -49,14 +49,14 @@ function switch_to_matchup_order() {
   $('matchup_button').disable();
   $('seed_button').enable();
   order = 'matchup';
-  apply_swaps(swaps);
+  apply_swaps(swaps.reverse());
 }
 
 function apply_swaps(swaps) {
   for(var region = 0; region < 4; ++region) {
-    swaps.forEach(function(swap) {
-      var index1 = swap[0] - 1;
-      var index2 = swap[1] - 1;
+    swaps.forEach(function(s) {
+      var index1 = s[0] - 1;
+      var index2 = s[1] - 1;
       var node1 = $('region_' + region + '_' + index1);
       var node2 = $('region_' + region + '_' + index2);
       node1.swap(node2);
