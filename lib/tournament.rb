d8b6903bd25e5e8720e727fde6009f624b139e8b
Douglas A. Seifert <doug@uptake.com>
2012-03-09 00:00:45 -0800

    bump version to 5.0.0

--- lib/tournament.rb
+++ lib/tournament.rb
@@ -7,7 +7,7 @@ unless defined? Tournament
 module Tournament
 
   # :stopdoc:
-  VERSION = '4.2.0'
+  VERSION = '5.0.0'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   # :startdoc:
